d6d9dea72e20fed70fdd3dc9b30b2693f4d53507
Ronald Martins <ronny_nascimento@hotmail.com>
2025-08-14 20:52:46 -0300

    Projeto Inicial

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,16 @@
+import './index.scss';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="page-title">Movie App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
